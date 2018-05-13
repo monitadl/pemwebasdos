ecfc4b857ec3ede35b0eb671903b35fa0943513b
monitadl <37854150+monitadl@users.noreply.github.com>
2018-05-13 13:07:32 +0700

    Add files via upload

---
+++ 2/content_footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs"></div>
+    <strong>Copyright &copy; <?php echo date("Y") ?> Unila Akademik and Created By Anggie, Monnita & NiPutu</strong>
+</footer>
